b62d68b8c81e5c6f78b75fce0ddb44b4ae3f44d9
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-12-18 13:43:44 -0300

    initial commit

---
+++ internal/api/user_handlers.go
@@ -0,0 +1,15 @@
+package api
+
+import "net/http"
+
+func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
+	panic("TODO - NOT IMPLEMENTED")
+}
+
+func (api *Api) handleLogin(w http.ResponseWriter, r *http.Request) {
+	panic("TODO - NOT IMPLEMENTED")
+}
+
+func (api *Api) handleLogout(w http.ResponseWriter, r *http.Request) {
+	panic("TODO - NOT IMPLEMENTED")
+}
