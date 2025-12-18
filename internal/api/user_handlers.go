a955b543ed93e08a5427660470c0a97afb9b86fc
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-12-18 17:53:32 -0300

    implements user creation

--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -1,9 +1,35 @@
 package api
 
-import "net/http"
+import (
+	"errors"
+	"gobid/internal/jsonutils"
+	"gobid/internal/services"
+	"gobid/internal/usecase/user"
+	"net/http"
+)
 
 func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
-	panic("TODO - NOT IMPLEMENTED")
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	id, err := api.UserService.Createuser(r.Context(), data.UserName, data.Email, data.Password, data.Bio)
+
+	if err != nil {
+		if errors.Is(err, services.ErrDuplicatedEmailOrPassword) {
+			_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, map[string]any{
+				"error": "email or password already exists",
+			})
+			return
+		}
+	}
+
+	_ = jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"user_id": id,
+	})
 }
 
 func (api *Api) handleLogin(w http.ResponseWriter, r *http.Request) {
---
+++ internal/usecase/user/create_user.go
@@ -0,0 +1,30 @@
+package user
+
+import (
+	"context"
+	"gobid/internal/validator"
+)
+
+type CreateUserReq struct {
+	UserName string `json:"user_name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Bio      string `json:"bio"`
+}
+
+func (req CreateUserReq) Valid(ctx context.Context) validator.Evaluator {
+	var eval validator.Evaluator
+
+	eval.CheckField(validator.NotBlank(req.UserName), "user_name", "this field cannot be empty")
+	eval.CheckField(validator.NotBlank(req.Email), "user_name", "this field cannot be empty")
+	eval.CheckField(validator.NotBlank(req.Bio), "user_name", "this field cannot be empty")
+	eval.CheckField(
+		validator.MinChars(req.Bio, 10) &&
+			validator.MaxChars(req.Bio, 255),
+		"bio",
+		"this field must have a lenght between 10 and 255",
+	)
+	eval.CheckField(validator.MinChars(string(req.Password), 8), "password", "password must be bigger than 8 chars")
+
+	return eval
+}
