4d8b60a24cfba13bb2552ffb9e1570b57572a563
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-12-20 10:16:01 -0300

    implements csrf

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -8,8 +8,16 @@ import (
 func (api *Api) BindRoutes() {
 	api.Router.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger, api.Sessions.LoadAndSave)
 
+	// csrfMiddleware := csrf.Protect(
+	// 	[]byte(os.Getenv("GOBID_CSRF_KEY")),
+	// 	csrf.Path("/api/v1"),
+	// 	csrf.Secure(true), // DEV ONLY
+	// )
+	// api.Router.Use(csrfMiddleware)
+
 	api.Router.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
+			// r.Get("/csrf_token", api.HandleGetCSFRToken)
 			r.Route("/users/", func(r chi.Router) {
 				r.Post("/signup", api.handleSignupUser)
 				r.Post("/login", api.handleLogin)
